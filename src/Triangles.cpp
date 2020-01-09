ce6187cee7d23af76959e2c5cdb114b3410cea18
PakTingHo <45459297+PakTingHo@users.noreply.github.com>
2020-01-09 15:57:25 +0000

    Triangle

--- src/Triangles.cpp
+++ src/Triangles.cpp
@@ -9,5 +9,13 @@ void Triangles::update(){
 }
 
 void Triangles::draw(){
-    
+	ofSetColor(0);
+	ofDrawTriangle(100, 200, 150, 200, 125, 250);
+	ofSetColor(100);
+	ofDrawTriangle(300, 50, 900, 300, 300, 600);
+	ofSetColor(255);
+	ofDrawTriangle(500,500,850,500,550,750);
+	ofSetColor(75);
+	ofDrawTriangle(150, 500, 75, 600, 225, 700);
+
 }
